9a5a8fa8461fdb2e2ae696579a8471a6513aa5aa
zexode <118525817+zexode@users.noreply.github.com>
2026-03-05 09:46:01 +0300

    добавила utils.py с функцие

---
+++ utils.py
@@ -0,0 +1,12 @@
+import json
+import os
+from config import TASKS_FILE
+
+def load_tasks():
+    if os.path.exists(TASKS_FILE):
+        with open(TASKS_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    return []
+
+
+
